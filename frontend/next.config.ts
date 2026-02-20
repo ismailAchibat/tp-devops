7466622ae1670c0d942e35391d99fe16eebc9cae
ismailAchibat <ismailachibat4@gmail.com>
2026-02-20 10:59:27 +0100

    création des Dockerfile pour le front et le back + l'orchestration avec docker-compose

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone", // pour un docker léger
 };
 
 export default nextConfig;
